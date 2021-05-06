2415a42bd5288f8f602599088351503a0a481768
ben12312 <benhurchrinstoper@gmail.com>
2021-05-07 01:39:56 +0700

    remove node_modules

--- monolith-server/routes/router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const movieRouter = require('./movieRouter');
-const tvSeriesRouter = require('./tvSeriesRouter');
-
-router.use('/movie', movieRouter);
-router.use('/tv-series', tvSeriesRouter);
-
-module.exports = router
